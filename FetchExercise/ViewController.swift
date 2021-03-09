7e7ca867beea3198cbf480424987a1a696342fba
Jon Duenas <jonduenas@icloud.com>
2021-03-09 10:48:29 -0800

    Initial Commit

---
+++ FetchExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchExercise
+//
+//  Created by Jon Duenas on 3/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
