2632f4c89be12398e45e3958788ecad36d3633b6
Jon Duenas <jonduenas@icloud.com>
2021-03-09 12:38:35 -0800

    Add iOS 12 compatibility to AppDelegate and SceneDelegate.

---
+++ FetchExercise/Lifecycle/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  FetchExercise
+//
+//  Created by Jon Duenas on 3/9/21.
+//
+
+import UIKit
+
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
