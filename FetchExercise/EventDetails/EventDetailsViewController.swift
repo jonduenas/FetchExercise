4c0040613d2ae8fc699d04c0bf9569e4be89a2fe
Jon Duenas <jonduenas@icloud.com>
2021-03-09 11:52:02 -0800

    Add EventDetailsViewController and attach outlets to storyboard.

---
+++ FetchExercise/EventDetails/EventDetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EventDetailsViewController.swift
+//  FetchExercise
+//
+//  Created by Jon Duenas on 3/9/21.
+//
+
+import UIKit
+
+class EventDetailsViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
